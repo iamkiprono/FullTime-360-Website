058ecb2dd28432ef935d53d612b07aeefca0efa2
Collins Kiprono Kirui <101986591+iamkiprono@users.noreply.github.com>
2023-02-06 18:11:42 -0800

    First Commit

---
+++ app.js
@@ -0,0 +1,18 @@
+fetch("news.json")
+  .then((res) => res.json())
+  .then((data) => {
+    
+    const render = data
+      .map((people) => {
+        console.log(people);
+        return `<div class="news-card">
+        <img src= ${people.image}>
+          <h3> ${people.news} </h3>
+          <p>${people.text}</p>
+          </div>`;
+      })
+      .join("");
+    console.log(render);
+
+    document.querySelector(".card").innerHTML = render;
+  });
