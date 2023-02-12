a5073189d55e6cc21e6d528761eb072289213296
Collins Kiprono Kirui <101986591+iamkiprono@users.noreply.github.com>
2023-02-12 12:03:10 -0800

    Updates
    Now you can stream live football matches from the site

--- livefootball.js
+++ livefootball.js
@@ -10,20 +10,15 @@ fetch("livematches.json")
     const matches = data.map((match) => {
       return (
         `<div class="live-game">
-          <div class="game">
-            <p class="team home">${match.home}</p>
-            <p>Vs</p>
-            <p class="team away">${match.away}</p>
-          </div>
-          <p class="game-link">
-            Link:
-            <a href=${match.link} target="_blank">
-              Watch here
-            </a>
-          </p>
-        </div>`
+        <div class="game">
+          <p class="team home"><img src=${match.logoHome} alt="">${match.home}</p>
+          <p class="vs">Vs</p>
+          <p class="team away"> <img src=${match.logoAway} alt="">${match.away}</p>
+        </div>
+        <p class="game-link">Link: <a href=${match.link} target="_blank">Watch here</a></p>
+      </div>`
       );
     });
 
-    match.innerHTML = matches;
+    match.innerHTML = matches.join(" ");
   });
