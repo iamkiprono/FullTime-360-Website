c264600d419a9677a9dae3c4cf3d41587e0e651d
Collins Kiprono Kirui <101986591+iamkiprono@users.noreply.github.com>
2023-04-30 14:02:19 +0300

    Updates

--- js/livefootball.js
+++ js/livefootball.js
@@ -4,7 +4,7 @@ const links = document.querySelector("a");
 
 const match = document.querySelector(".matches");
 
-fetch("/livematches.json")
+fetch("https://blog-api-kiprono.onrender.com/live")
   .then((response) => response.json())
   .then((data) => {
     if (data.response == undefined) {
@@ -12,11 +12,11 @@ fetch("/livematches.json")
       const matches = data.map((match) => {
         return `<div class="live-game">
           <div class="game">
-            <p class="team home"><img src=${match.logoHome} alt="">${match.home}</p>
+            <p class="team home"><img src=${match.homelogo} alt="">${match.hometeam}</p>
             <p class="vs">Vs</p>
-            <p class="team away"> <img src=${match.logoAway} alt="">${match.away}</p>
+            <p class="team away"> <img src=${match.awaylogo} alt="">${match.awayteam}</p>
           </div>
-          <p class="game-link">Link: <a class="match-link" href=${match.link} target="_blank">Watch here</a></p>
+          <p class="game-link">Link: <a class="match-link" href=${match.matchlink} target="_blank">Watch here</a></p>
         </div>`;
       })
       match.innerHTML = matches.join(" ");
